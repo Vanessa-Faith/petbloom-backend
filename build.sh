d53a7dfa83eb1f217cb978f0490446301576a9c0
Vanessa <vanessa.omondi@student.moringaschool.com>
2026-01-19 12:48:28 +0300

    Simplify build script for Render

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-echo "Installing system dependencies..."
-apt-get update
-apt-get install -y postgresql-client
-
 echo "Installing Python packages..."
 pip install --upgrade pip
 pip install -r requirements.txt
